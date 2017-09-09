f530584ac023684823a1b45c54d58bfbb58db64e
Alex Alves <alex7alves10@hotmail.com>
2017-09-09 11:50:34 -0300

    Melhorando função

--- PSO.cpp
+++ PSO.cpp
@@ -2,8 +2,17 @@
 /*
     Algoritmo de nuvem de particulas
     Autor : Alex Alves
+
+
+    Posição atual de x e y =
+    Nuvem_particula[i][0] e Nuvem_particula[i][1]
+    melhor Posição  de x e y =
+    Nuvem_particula[i][2] e Nuvem_particula[i][3]
+    Velocidade  de x e y =
+    Nuvem_particula[i][4] e Nuvem_particula[i][5]
+    f(p) = Nuvem_particula[i][6]
 */
-#include <iosream>
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -13,7 +22,29 @@ int Minimo (int Tamanho,float m[][7]);
 
 int main ()
 {
+    int duracao = 50; // iterações maxima
+    float w = 0.7; // ponderação da inercia
+    float c= 1.2; // parametro cognitivo
+    srand(time(NULL));
+    int Tamanho_populacao = 30,gbest;
+    //Criando a população
+    float Nuvem_particula[Tamanho_populacao][7];
+    float x,y,saidafuncao,r1,r2;
 
+
+    //Iniciando a população
+    for (int i = 0; i< Tamanho_populacao;i++){
+        for (int j = 0; j<7;j++){
+            Nuvem_particula[i][j] = i+1;
+        }
+    }
+    /*for (int i = 0; i< Tamanho_populacao;i++){
+        for (int j = 0; j<7;j++){
+            cout << Nuvem_particula[i][j]<< " ";
+        }
+        cout << endl;
+    }*/
+    gbest = Minimo(Tamanho_populacao,Nuvem_particula);
 
     return 0;
 }
@@ -23,8 +54,8 @@ int Minimo (int Tamanho,float m[][7]){
     float minimo = m[0][6];
 
     for (int i = 0; i< Tamanho;i++){
-       if(minimo > m[i][7]){
-            minimo = m[i][7];
+       if(minimo > m[i][6]){
+            minimo = m[i][6];
             pos =i;
        }
     }
