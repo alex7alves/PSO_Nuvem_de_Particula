35a22634269fa45596039bdc9cf9be6b00b569b9
Alex Alves <alex7alves10@hotmail.com>
2017-09-09 14:25:10 -0300

    Finalizando algoritmo

--- PSO.cpp
+++ PSO.cpp
@@ -25,8 +25,8 @@ int main ()
 {
     srand(time(NULL));
     int duracao = 50; // iterações maxima
-    float w = 0.7; // ponderação da inercia
-    float c= 1.2,c2=1.4; // parametro cognitivo
+    float w = 0.5; // ponderação da inercia
+    float c= 1.5,c2=1.5; // parametros cognitivos
     int Tamanho_populacao = 30,gbest;
     //Criando a população
     float Nuvem_particula[Tamanho_populacao][7];
@@ -44,17 +44,39 @@ int main ()
         Nuvem_particula[i][5] = 0;   //  Velocidade inicial para y
         Nuvem_particula[i][6] = 1000; // valor inicial  maximo para função objetivo
     }
-
-  /*  for (int i = 0; i< Tamanho_populacao;i++){
-        for (int j = 0; j<7;j++){
-            cout << Nuvem_particula[i][j]<< " ";
-        }
-        cout << endl;
-    }*/
-
     for (int iter=1;iter<=duracao;iter++){
 
         for(int i =0;i<Tamanho_populacao;i++){
+            // Atualizando as posições das particulas
+             Nuvem_particula[i][0]=Nuvem_particula[i][0] +Nuvem_particula[i][4];
+             Nuvem_particula[i][1] = Nuvem_particula[i][1] + Nuvem_particula[i][5];
+             // Fazendo restrições para as posições
+            if ( Nuvem_particula[i][0] < -500) {
+                 Nuvem_particula[i][0] =-500;
+            }
+            if (Nuvem_particula[i][1] < -500) {
+                Nuvem_particula[i][1] = -500;
+            }
+             if ( Nuvem_particula[i][0] > 500) {
+                 Nuvem_particula[i][0] =500;
+            }
+            if (Nuvem_particula[i][1] > 500) {
+                Nuvem_particula[i][1] = 500;
+            }
+            // fazendo restrições para as velocidades
+            if ( Nuvem_particula[i][4] > 30) {
+                 Nuvem_particula[i][4] =30;
+            }
+            if (Nuvem_particula[i][5] > 30) {
+                Nuvem_particula[i][5] = 30;
+            }
+             if ( Nuvem_particula[i][4] < -30) {
+                 Nuvem_particula[i][4] =-30;
+            }
+            if (Nuvem_particula[i][5] < -30) {
+                Nuvem_particula[i][5] = -30;
+            }
+
             x= Nuvem_particula[i][0];
             y= Nuvem_particula[i][1];
             saidafuncao =(x*x) + (y*y)+ ((3*x)+ (4*y)-26)*((3*x)+ (4*y)-26);
@@ -68,6 +90,8 @@ int main ()
                 // Pegar melhor posição do grupo
                 gbest = Minimo(Tamanho_populacao,Nuvem_particula);  // equivale a f(x) <f(g)
             }
+           // gbest = Minimo(Tamanho_populacao,Nuvem_particula);
+        }
             for (int j=0;j<Tamanho_populacao;j++){
                  r1= Randomico();
                  r2= Randomico();
@@ -76,17 +100,13 @@ int main ()
                  r1= Randomico();
                  r2= Randomico();
                  Nuvem_particula[j][5] = (w*Nuvem_particula[j][5]) + (c*r1*(Nuvem_particula[j][3] -Nuvem_particula[j][1])) + (c2*r2*(Nuvem_particula[gbest][3] -Nuvem_particula[j][1]));
-              //  cout <<  Nuvem_particula[j][4] << " "  << Nuvem_particula[j][5] ;
-            }
-            // Atualizando as posições das particulas
-             Nuvem_particula[i][0]=Nuvem_particula[i][0] +Nuvem_particula[i][4];
-             Nuvem_particula[i][1] = Nuvem_particula[i][1] + Nuvem_particula[i][5];
-   }
-        cout <<  Nuvem_particula[iter][0] << " "  << Nuvem_particula[iter][1] << " " << endl ;
-    //   cout <<  Nuvem_particula[iter][4] << " "  << Nuvem_particula[iter][5] << " " << endl ;
-    }
-    //gbest = Minimo(Tamanho_populacao,Nuvem_particula);
 
+            }
+     }
+     for (int i =0 ;i <Tamanho_populacao;i++){
+         cout <<  Nuvem_particula[i][0] << " "  << Nuvem_particula[i][1] << " " << endl ;
+           //   cout <<  Nuvem_particula[i][4] << " "  << Nuvem_particula[i][5] << " " << endl ;
+     }
     return 0;
 }
 
@@ -100,7 +120,6 @@ int Minimo (int Tamanho,float m[][7]){
             pos =i;
        }
     }
-  // cout << minimo << "  " << pos;
     return pos;
 }
 
